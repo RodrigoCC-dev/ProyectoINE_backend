f2a6ef69854b34726b5e6b1da97de68c034b09ee
Rodrigo <rodrigo.castillo.c@usach.cl>
2020-01-07 05:26:56 -0300

    Agregados métodos findBy en RepositoryPersona
    Se agregan métodos findByComuna y findByProvincia

--- src/main/java/cl/usach/fingesoft/repository/RepositoryPersona.java
+++ src/main/java/cl/usach/fingesoft/repository/RepositoryPersona.java
@@ -10,7 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import cl.usach.fingesoft.model.Comuna;
 import cl.usach.fingesoft.model.Persona;
+import cl.usach.fingesoft.model.Provincia;
 
 @Component
 public class RepositoryPersona {
@@ -57,4 +59,73 @@ public class RepositoryPersona {
 		}
 		return personas;
 	}
+	
+	public List<Persona> findByComuna(String nombre){
+		List<Persona> personas = new ArrayList<>();
+		String nombreArchivo = "Microdato_Censo2017-Personas.csv";
+		String nombreFuente = "Personas_";
+		String texto = "";
+		String[] info;
+		Comuna comuna = repoComuna.findDatos(nombre);
+		if(comuna.getNumero() != 0) {
+			nombreFuente = nombreFuente + comuna.getNombre() + ".csv";
+			try {
+				FileReader archivo = new FileReader(repoArchivos.getRutaPrincipal() + repoArchivos.getRutaPersonas() + nombreFuente);
+				BufferedReader contenido = new BufferedReader(archivo);
+				texto = contenido.readLine();
+				while((texto = contenido.readLine()) != null) {
+					info = texto.split(";");
+					Persona nuevaPersona = new Persona(info);
+					personas.add(nuevaPersona);
+				}
+				contenido.close();
+			}
+			catch (Exception e) {
+				try {
+					FileReader archivo = new FileReader(repoArchivos.getRutaPrincipal() + repoArchivos.getRutaPersonas() + nombreArchivo);
+					BufferedReader contenido = new BufferedReader(archivo);
+					texto = contenido.readLine();
+					while((texto = contenido.readLine()) != null) {
+						info = texto.split(";");
+						if(Integer.parseInt(info[2]) == comuna.getNumero()) {
+							Persona nuevaPersona = new Persona(info);
+							personas.add(nuevaPersona);
+						}
+					}
+					contenido.close();
+					repoArchivos.guardarPersonasPorComuna(nombre, repoArchivos.getRutaPrincipal() + repoArchivos.getRutaPersonas());					
+				}
+				catch (Exception ex) {
+					LOG.error("Error con findByComuna. No es posible abrir archivo " + nombreArchivo);
+				}
+			}
+		}
+		else {
+			LOG.warn("No existe la comuna " + nombre);
+		}
+		return personas;	
+	}
+	
+	
+	
+	public List<Persona> findByProvincia(String provincia){
+		Provincia nuevaProv = repoProvincia.findDatos(provincia);
+		nuevaProv = repoProvincia.findComunas(nuevaProv);
+		List<Persona> personas = new ArrayList<>();
+		String nombreComuna = "";
+		if(nuevaProv.getNumero() != 0) {
+			for(int i = 0; i < nuevaProv.getListaComunas().size(); i++) {
+				nombreComuna = nuevaProv.getListaComunas().get(i).getNombre();
+				personas.addAll(this.findByComuna(nombreComuna));
+			}
+		}
+		else {
+			LOG.error("Error con findByProvincia. No se encuentra la provincia " + provincia);
+		}
+		return personas;
+	}
+	
+	
+	
+	
 }
